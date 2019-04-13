0d607716cf9eed512c4b5b2fa243352ae92ed603
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-04-13 14:12:28 -0500

    Pseudocode for all the compponents

--- src/component/Register.js
+++ src/component/Register.js
@@ -1,3 +1,3 @@
-//import react from react 
+import React from "react" 
 //create a function that called "userRegistration" which return the registration logics
 //export "User" regisration to the Post.js
