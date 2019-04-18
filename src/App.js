a0cf6f59a8f505d7def6ab63e12eea4f55b157ad
Robert O <romohund@gmail.com>
2019-04-17 20:57:40 -0500

    Rendered login page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Login from "./component/Login.js"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-         Swerve main page
-                 </header>
+     
+      <Router>
+      <div>
+        
+     
+          <Route exact path="/login" component={Login} />
+          {/* <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/post" component={Post} />
+          <Route exact path="/filter" component={Filter} />
+          <Route component={NoMatch} /> */}
+        
       </div>
+    </Router>
     );
+    
   }
 }
+
 
 export default App;
