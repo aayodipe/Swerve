2213cf0e5c6dcf2ecc5da46e92184931f8709be9
Robert O <romohund@gmail.com>
2019-04-23 22:33:51 -0500

    Code on Post.js added but unable to test

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,6 +1,54 @@
-//import react and Component from react
-//import Login from "./Login"
-//import Register from "./Register"
+import React, { Component } from "react";
+import Login from "./Login";
+import Register from "./Register";
 //Create the function to "UserPost" which  returns user post.
 //NOTE: THIS IS WHERE THE USER WILL WRITE WHATEVER THE WOULD LIKE TO POST AND WHEN THE SUBMIT THEIR POST IT SEND TO THE DATABASE
 //Export Post to the Container
+
+export default class FormReport extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        location: "",
+        description:""
+      };
+  
+      this.handleInputChange = this.handleInputChange.bind(this);
+    }
+  
+    handleInputChange(event) {
+      const target = event.target;
+      const value = target.type === 'text' ? target.checked : target.value;
+      const name = target.name;
+  
+      this.setState({
+        [name]: value
+      });
+    }
+  
+    render() {
+      return (
+        <form>
+          <label>
+            Location:
+            <input
+              name="location"
+              type="text"
+              checked={this.state.location}
+              onChange={this.handleInputChange} />
+          </label>
+          <br />
+          <label>
+            Number of guests:
+            <input
+              name="description"
+              type="text"
+              value={this.state.description}
+              onChange={this.handleInputChange} />
+          </label>
+        </form>
+      );
+    }
+  }
+
+  module.exports = Post.js;
