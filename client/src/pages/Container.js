5045fecee330f73b84aac7c0e4a23d93173c1489
SMH <shannonmhenry1@gmail.com>
2019-04-29 20:30:26 -0500

    updated post page

--- client/src/pages/Container.js
+++ client/src/pages/Container.js
@@ -3,14 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./Login"
 import Register from "./Register"
 import Dashboard from "./Dashboard"
-<<<<<<< HEAD
-import Post from "./Post"
-import Filter from "./Filter"
-
-=======
 import Navbar from './Navbar'
 import Post from './Post'
->>>>>>> 9a598cb2883dee03a6211c9c221c8d643753c2dd
 class Container extends Component {
   render() {
     return (
@@ -20,21 +14,11 @@ class Container extends Component {
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/dashboard" component={Dashboard} />
-<<<<<<< HEAD
-          <Route exact path="/post" component={Post} />
-          <Route exact path="/filter" component={Filter} />
-          </div>
-          {/*<Route exact path="/post" component={Post} />
-          <Route exact path="/filter" component={Filter} />
-          <Route component={NoMatch} /> */}
-=======
           
          <Route exact path="/post" component={Post} />
           </div>
           {/* <Route exact path="/filter" component={Filter} />
           <Route component={NoMatch} /> */} */}
-        </div>
->>>>>>> 9a598cb2883dee03a6211c9c221c8d643753c2dd
      
     </Router>
     );
