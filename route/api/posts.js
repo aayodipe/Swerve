09acbd5a83361f6026050e696cbf8f11b16b3eaf
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-05-01 00:56:50 -0500

    Added the route to get all post and find and individual post

--- route/api/posts.js
+++ route/api/posts.js
@@ -1,10 +1,82 @@
-const express = require('express')
- const router = express.Router()
+ const express = require('express');
+ const router = express.Router();
+ const { check, validationResult } = require('express-validator/check');
+ const auth = require('../../middleware/auth');
+  //import model
+ const Post = require('../../models/Post');
+ const User = require('../../models/User');
 
- //@routes Get api/post
- //@ desc Test route
- //@ access Public
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
- router.get('/', (req,res)=>res.json('Post route'))
+    try {
+      const user = await User.findById(req.user.id).select('-password');
 
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        image: user.image,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Private
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
+// @route    GET api/posts/:id
+// @desc     Get post by ID
+// @access   Private
+router.get('/:id', async (req, res) => {
+     try {
+       const post = await Post.findById(req.params.id);
+   
+       if (!post) {
+         return res.status(404).json({ msg: 'Post not found' });
+       }
+   
+       res.json(post);
+     } catch (err) {
+       console.error(err.message);
+       if (err.kind === 'ObjectId') {
+         return res.status(404).json({ msg: 'Post not found' });
+       }
+       res.status(500).send('Server Error');
+     }
+   });
  module.exports = router
