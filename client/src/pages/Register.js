7e23a75b428171174ee7786fe2cb33deed8fc1b7
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-05-04 08:53:09 -0500

    Modified Registeration form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -7,61 +7,72 @@ import API from "../util/API";
 
 
 
-    export default class Register extends Component{
-        constructor(props) {
-             super(props);
-     
-             this.state = {
-                 name: "",
-                 email: "",
-                 password: ""
-             };
-         }
-     
-         //Function to allow data in the form fields
-         validateForm() {
-             return this.state.name.length > 0 && this.state.password.length > 0;
-         }
-   //Clear from input
-         clearFormInput(){
-            this.setState({ 
-                name: "",
-                email: "",
-                password: "" })}
+export default class Register extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            email: "",
+            password: "",
+            password2:""
+        };
+    }
+
+    //Function to allow data in the form fields
+    // validateForm() {
+    //     return this.state.name.length > 0 && this.state.password.length > 0;
+    // }
+//Clear from input
+    clearFormInput(){
+       this.setState({ 
+           name: "",
+           email: "",
+           password: "",
+           password2:"" })}
+    
+    //Handle Form Change
+    handleInputChange = event => {
+      const { name, value } = event.target;
+      this.setState({
+        [name]: value
+      });
+    };
+
+
+//Handle Form Submit
+    handleFormSubmit = event => {
+      event.preventDefault();
+      if (!this.state.email){
+          alert('Please enter an email!')
+      }else 
+      if(this.state.password  !== this.state.password2){
+          alert('Password do not match!')
+      }else{
+          //Save to User's database
+        API.saveUser({
+          name: this.state.name,
+          email: this.state.email,
+          password: this.state.password,
          
-         //Handle Form Change
-         handleInputChange = event => {
-           const { name, value } = event.target;
-           this.setState({
-             [name]: value
-           });
-         };
+        })
+           .then(
+                res => {
+                   
+                this.clearFormInput()
+                console.log(res)
 
-     
-     //Handle Form Submit
-         handleFormSubmit = event => {
-           event.preventDefault();
-           if (this.state.email) {
-             API.saveUser({
-               username: this.state.name,
-               email: this.state.email,
-               password: this.state.password
-             })
-               .then(
-                   res => {res.json('Registration successful!')
-                   this.clearFormInput()
-                console.log(res.data)}
-               )
-               .catch(err => console.log(err));
-           }
-         };
-
+            })
+            .catch(err => console.log(err.message));
+      }
+    };
     render() {
         return (
             <div className="Register">
                 <div className="header">
                     <div className="headerWaves"></div>
-                    <h1 className="headerFont">SWERVE</h1>
+                    <h1 className="headerFont">User Registration</h1>
                 </div>
                 <form>
                     <Form.Group controlId="name" bsSize="large">
@@ -72,6 +83,17 @@ import API from "../util/API";
                             name="name"
                             value={this.state.name}
                             onChange={this.handleInputChange}
+                            placeholder = "Name"
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="email" bsSize="large">
+                        <Form.Label className="normal-font">Email Address</Form.Label>
+                        <Form.Control
+                            name = "email"
+                            value={this.state.email}
+                            onChange={this.handleInputChange}
+                            type="email"
+                            placeholder ="example@yourname.com"
                         />
                     </Form.Group>
                     <Form.Group controlId="password" bsSize="large">
@@ -82,24 +104,25 @@ import API from "../util/API";
                             type="password"
                             value={this.state.password}
                             onChange={this.handleInputChange}
+                            placeholder ="Password must be min of letter"
                         />
+                    
                     </Form.Group>
-                    <Form.Group controlId="email" bsSize="large">
-                        <Form.Label className="normal-font">Email Address</Form.Label>
+                    <Form.Group controlId="password2" bsSize="large">
+                        <Form.Label className="normal-font"> Password2 </Form.Label>
                         <Form.Control
-                            name="email"
-                            type="email"
-                            value={this.state.email}
+                            autoFocus
+                            type="password"
+                            name= "password2"
+                            value={this.state.password2}
                             onChange={this.handleInputChange}
-                            
+                            placeholder = "Must match with password"
                         />
+                    
                     </Form.Group>
-
-                    <Button className="custom-btn" 
-                    type = "submit"
-                    onClick={this.handleFormSubmit}>
-                        Sign up
-
+                   
+                    <Button block className="custom-btn" type="submit" onClick = {this.handleFormSubmit}>
+                        Sign Up
                     </Button>
                 </form>
             </div>
