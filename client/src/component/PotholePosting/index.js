66bc096b72b601c1354e953f14063fb0f9101c62
SMH <shannonmhenry1@gmail.com>
2019-05-04 14:27:54 -0500

    separate containersgit add .git add .

---
+++ client/src/component/PotholePosting/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+function PotholePosting(props) {
+    console.log(props);
+    const { post } = props;
+    return (
+        <div className="card">
+            <h1>DanTheMan</h1>
+            <h2>{post.location}</h2>
+            <small>{post.date}</small>
+            <p>{post.description}</p>
+            <p>0 Notes</p>
+        </div>
+    )
+}
+
+export default PotholePosting;
