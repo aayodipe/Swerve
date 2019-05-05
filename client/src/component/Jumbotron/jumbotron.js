b34437b36fd13c6fc0bc06e3e79e4dd7b398a9cd
Robert O <romohund@gmail.com>
2019-05-04 21:18:40 -0500

    Rendered buttons to Dashboard page and some modal work

---
+++ client/src/component/Jumbotron/jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div
+      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+      className="jumbotron"
+    >
+    </div>
+  );
+}
+
+{Jumbotron()};
+
+export default Jumbotron;
