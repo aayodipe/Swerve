4919c1d2347c7b7972707f661d824494f8fa447f
SMH <shannonmhenry1@gmail.com>
2019-05-06 20:37:58 -0500

    edited svg

--- client/src/component/DashboardHeader.js
+++ client/src/component/DashboardHeader.js
@@ -8,16 +8,16 @@ export default class DashboardHeader extends Component {
         return (
             <div className="swerve-header">
                 <h1 className="header-text">SWERVE</h1>
-                <div className="username-logo">
+                {/* <div className="username-logo">
                     <img src={logo} alt="user icon" className="logo" />
                     <h1 className="username-text">Logged In Username</h1>
-                </div>
-                <div className="box">
+                </div> */}
+                {/* <div className="box">
                     <div className="header-waves"></div>
-                </div>
-                <div class="container">
-                    <svg viewBox="-10 0 900 500" preserveAspectRatio="xMinYMin meet">
-                        <path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" className="wave"></path>
+                </div> */}
+                <div className="svg-container">
+                    <svg viewBox="0 0 4500 200">
+                        <path d="M0,100 C150,200 350,0 500,100 C650,0 700,100 850,200 C1000,100 1150,0 1300,100 C1450,200 1600,100 1750,0 C1900,100 2050,200 2200,100 C2350,0 2500,100 2650,200 L2600,00 L0,0 Z" className="wave"></path>
                     </svg>
                 </div>
             </div>
