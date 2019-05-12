69a2926473676fab019c0d6ae36ad94a367f6b03
SMH <shannonmhenry1@gmail.com>
2019-05-12 02:27:59 -0500

    added basic filter page

--- client/src/component/DashboardFooter/DashboardFooter.js
+++ client/src/component/DashboardFooter/DashboardFooter.js
@@ -7,7 +7,7 @@ export default class DashboardFooter extends Component {
         return (
             <div className="footer-container">
                 <div className="footer-links">
-                    {/* <h1><a href="/filter" className="link-text">Filter</a></h1> */}
+                    <h1><a href="/filter" className="link-text">Filter</a></h1>
                     <h1><a href="/post" className="link-text">Post</a></h1>
                 </div>
             </div>
