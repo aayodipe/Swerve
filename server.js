4d80dbc4075a6243700e669c5dbfdd10a7d4249e
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-05-16 14:14:48 -0500

    heroku debug

--- server.js
+++ server.js
@@ -21,8 +21,12 @@ app.use('/api/auth', authRoute)
 connectDB()
 
 if(process.env.NODE_ENV === 'production'){
-     app.use(express.static('client/build'))
-}
+     //set static folder
+     app.use(express.static('client/build'));
+ }
+ app.get('*',(req, res) => {
+     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+ });
 
 app.listen(PORT, ()=>{
      console.log( `Server Listen to port ${PORT}`)
