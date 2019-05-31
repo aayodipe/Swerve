6d2d7e9b93286fb55b1835ff74434e1ee04691d3
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-05-30 20:53:15 -0500

    updated package.json

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ const userRoute= require('./routes/api/users')
 const  authRoute= require('./routes/api/auth')
 const postRoute = require('./routes/api/posts')
 const mongoose = require('mongoose');
-const mongo_URL = ``
-
-
 
 // initialize middleware 
 app.use(express.urlencoded({ extended: true }));
@@ -28,10 +25,14 @@ if(process.env.NODE_ENV === 'production'){
      app.use(express.static('client/build'));
 };
 
+app.get('*', (req, res) => {
+     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+   });
 // Connect to the Mongo DB
-mongoose.connect(process.env.Mongo_URL || 'mongodb+srv://aayodipe:Adeyemi2@cluster0-xcjje.mongodb.net/test?retryWrites=true', {useNewUrlParser: true}).then(resp =>{
-    app.listen(PORT, ()=>{
-        console.log( `Server Listen to port ${PORT}`)
-   })
+mongoose.connect(process.env.Mongo_URL || 'mongodb+srv://aayodipe:Adeyemi2@cluster0-xcjje.mongodb.net/test?retryWrites=true', {useNewUrlParser: true}
+   
+)
+app.listen(PORT, ()=>{
+     console.log( `Server Listen to port ${PORT}`)
 })
 
