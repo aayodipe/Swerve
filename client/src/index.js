dc77456dd0fc6de43b1417b11e9e4c5f3aeb3106
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2019-05-30 21:30:34 -0500

    updated package.json

--- client/src/index.js
+++ client/src/index.js
@@ -4,23 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { Security } from '@okta/okta-react';
 
 import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
-
-const oktaConfig = {
-  issuer: `${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`,
-  redirect_uri: `${window.location.origin}/implicit/callback`,
-  client_id: process.env.REACT_APP_OKTA_CLIENT_ID,
-};
 
 ReactDOM.render(
   <BrowserRouter>
-    <Security {...oktaConfig}>
-      <App />
-    </Security>
+         <App />
+
   </BrowserRouter>,
   document.getElementById('root'),
 );
-//registerServiceWorker();
-
-if (module.hot) module.hot.accept();
-
